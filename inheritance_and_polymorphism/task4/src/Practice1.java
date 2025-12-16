f1da7bdd765d0cb11889b9f0a6d592804bf021aa
Vitalia <taalityx@gmail.com>
2025-12-17 01:52:50 +0300

    task 4 from module 4 completed

---
+++ inheritance_and_polymorphism/task4/src/Practice1.java
@@ -0,0 +1,18 @@
+public class Practice1 {
+    static public class Calculator {
+        // Метод для сложения двух целых чисел
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+        // Метод для сложения трех целых чисел
+        public int add(int a, int b, int c) {
+            return a + b + c;
+        }
+
+        // Метод для сложения двух чисел с плавающей запятой
+        public double add(double a, double b) {
+            return a + b;
+        }
+    }
+}
